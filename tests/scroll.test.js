774007fca9d135d0c26099b001e94b3c0890f210
zombiej <smith3816@gmail.com>
2020-08-13 11:01:37 +0800

    clean up

--- tests/scroll.test.js
+++ tests/scroll.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import List from '../src';
-import { spyElementPrototypes } from './utils/domHook';
 
 function genData(count) {
   return new Array(count).fill(null).map((_, index) => ({ id: String(index) }));
@@ -39,113 +38,5 @@ describe('List.Scroll', () => {
       jest.runAllTimers();
       expect(wrapper.find('ul').instance().scrollTop).toEqual(903);
     });
-  });
-
-  describe('scrollTo item index', () => {
-    let mockElement;
-    let scrollTop = 0;
-
-    beforeAll(() => {
-      mockElement = spyElementPrototypes(HTMLElement, {
-        offsetHeight: {
-          get: () => 20,
-        },
-        clientHeight: {
-          get: () => 100,
-        },
-        scrollHeight: {
-          get: () => 400,
-        },
-        scrollTop: {
-          get: () => scrollTop,
-          set(_, val) {
-            scrollTop = val;
-          },
-        },
-      });
-    });
-
-    afterAll(() => {
-      mockElement.mockRestore();
-    });
-
-    function testPlots(type, scrollConfig, props) {
-      describe(`${type} list`, () => {
-        let listRef;
-        let wrapper;
-        const onScroll = jest.fn();
-
-        beforeEach(() => {
-          onScroll.mockReset();
-          listRef = React.createRef();
-          wrapper = genList({
-            itemHeight: 20,
-            height: 100,
-            data: genData(20),
-            className: 'list',
-            ref: listRef,
-            onScroll,
-            ...props,
-          });
-        });
-
-        it('not crash', () => {
-          listRef.current.scrollTo({ ...scrollConfig, align: 'top' });
-          wrapper.unmount();
-          jest.runAllTimers();
-        });
-
-        it('top', () => {
-          listRef.current.scrollTo({ ...scrollConfig, align: 'top' });
-          jest.runAllTimers();
-          expect(scrollTop).toEqual(200);
-        });
-        it('bottom', () => {
-          listRef.current.scrollTo({ ...scrollConfig, align: 'bottom' });
-          jest.runAllTimers();
-          expect(scrollTop).toEqual(120);
-        });
-        describe('auto', () => {
-          it('upper of', () => {
-            scrollTop = 210;
-            wrapper
-              .find('.list')
-              .last()
-              .simulate('scroll');
-            expect(onScroll).toHaveBeenCalled();
-            listRef.current.scrollTo({ ...scrollConfig, align: 'auto' });
-            jest.runAllTimers();
-            expect(scrollTop).toEqual(200);
-          });
-          it('lower of', () => {
-            scrollTop = 110;
-            wrapper
-              .find('.list')
-              .last()
-              .simulate('scroll');
-            expect(onScroll).toHaveBeenCalled();
-            listRef.current.scrollTo({ ...scrollConfig, align: 'auto' });
-            jest.runAllTimers();
-            expect(scrollTop).toEqual(120);
-          });
-          it('in range', () => {
-            scrollTop = 150;
-            wrapper
-              .find('.list')
-              .last()
-              .simulate('scroll');
-            expect(onScroll).toHaveBeenCalled();
-            listRef.current.scrollTo({ ...scrollConfig, align: 'auto' });
-            jest.runAllTimers();
-            expect(scrollTop).toEqual(150);
-          });
-        });
-      });
-    }
-
-    testPlots('virtual list', { index: 10 });
-    testPlots('raw list', { index: 10 }, { itemHeight: null });
-    testPlots('virtual list by key', { key: '10' });
-    testPlots('raw list by key', { key: '10' }, { itemHeight: null });
   });
 });
