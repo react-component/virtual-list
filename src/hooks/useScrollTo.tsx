ef5a0740bb36bb03f16b7037c0058aa31a50abca
zombiej <smith3816@gmail.com>
2020-08-13 18:07:01 +0800

    chore: scrollTo will trigger at once

--- src/hooks/useScrollTo.tsx
+++ src/hooks/useScrollTo.tsx
@@ -31,45 +31,43 @@ export default function useScrollTo<T>(
 
       // We will retry 3 times in case dynamic height shaking
       const syncScroll = (times = 3) => {
-        if (times < 0) return;
+        if (times < 0 || !containerRef.current) return;
 
-        scrollRef.current = raf(() => {
-          if (!containerRef.current) return;
+        // Get top & bottom
+        let stackTop = 0;
+        let itemTop = 0;
+        let itemBottom = 0;
 
-          // Get top & bottom
-          let stackTop = 0;
-          let itemTop = 0;
-          let itemBottom = 0;
+        for (let i = 0; i <= index; i += 1) {
+          const key = getKey(data[i]);
+          itemTop = stackTop;
+          const cacheHeight = heights.get(key);
+          itemBottom = itemTop + (cacheHeight === undefined ? itemHeight : cacheHeight);
 
-          for (let i = 0; i <= index; i += 1) {
-            const key = getKey(data[i]);
-            itemTop = stackTop;
-            const cacheHeight = heights.get(key);
-            itemBottom = itemTop + (cacheHeight === undefined ? itemHeight : cacheHeight);
+          stackTop = itemBottom;
+        }
 
-            stackTop = itemBottom;
-          }
+        // Scroll to
+        switch (align) {
+          case 'top':
+            containerRef.current.scrollTop = itemTop;
+            break;
+          case 'bottom':
+            containerRef.current.scrollTop = itemBottom - height;
+            break;
 
-          // Scroll to
-          switch (align) {
-            case 'top':
+          default: {
+            const { scrollTop } = containerRef.current;
+            const scrollBottom = scrollTop + height;
+            if (itemTop < scrollTop) {
               containerRef.current.scrollTop = itemTop;
-              break;
-            case 'bottom':
+            } else if (itemBottom > scrollBottom) {
               containerRef.current.scrollTop = itemBottom - height;
-              break;
-
-            default: {
-              const { scrollTop } = containerRef.current;
-              const scrollBottom = scrollTop + height;
-              if (itemTop < scrollTop) {
-                containerRef.current.scrollTop = itemTop;
-              } else if (itemBottom > scrollBottom) {
-                containerRef.current.scrollTop = itemBottom - height;
-              }
             }
           }
+        }
 
+        scrollRef.current = raf(() => {
           syncScroll(times - 1);
         });
       };
