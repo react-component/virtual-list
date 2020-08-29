68b7d831d32e6199fde1f50a8cbe7046824f57cb
zombiej <smith3816@gmail.com>
2020-08-29 21:49:35 +0800

    smooth it

--- src/hooks/useMobileTouchMove.ts
+++ src/hooks/useMobileTouchMove.ts
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useRef } from 'react';
 
-const SMOOTH_PTG = 14 / 15;
+const SMOOTH_PTG = 19 / 20;
 
 export default function useMobileTouchMove(
   listRef: React.RefObject<HTMLDivElement>,
@@ -10,28 +10,15 @@ export default function useMobileTouchMove(
   const touchedRef = useRef(false);
   const touchYRef = useRef(0);
 
+  const elementRef = useRef<HTMLElement>(null);
+
   // Smooth scroll
   const intervalRef = useRef(null);
 
-  const onTouchStart = (e: TouchEvent) => {
-    if (e.touches.length === 1 && !touchedRef.current) {
-      touchedRef.current = true;
-      touchYRef.current = Math.ceil(e.touches[0].screenY);
-      console.log('Start >>>', touchedRef.current);
-    } else {
-      console.log('Start >>> Skip');
-    }
-
-    if (touchedRef.current) {
-      // e.preventDefault();
-    }
-  };
+  let cleanUpEvents: () => void;
 
   const onTouchMove = (e: TouchEvent) => {
-    console.log('Move >>>', touchedRef.current);
     if (touchedRef.current) {
-      // e.preventDefault();
-
       const currentY = Math.ceil(e.touches[0].screenY);
       let offsetY = touchYRef.current - currentY;
       touchYRef.current = currentY;
@@ -40,34 +27,50 @@ export default function useMobileTouchMove(
 
       // Smooth interval
       clearInterval(intervalRef.current);
-      // intervalRef.current = setInterval(() => {
-      //   offsetY *= SMOOTH_PTG;
-      //   callback(offsetY);
+      intervalRef.current = setInterval(() => {
+        offsetY *= SMOOTH_PTG;
+        callback(offsetY);
 
-      //   if (Math.abs(offsetY) <= 0.2) {
-      //     clearInterval(intervalRef.current);
-      //   }
-      // }, 10);
+        if (Math.abs(offsetY) <= 0.1) {
+          clearInterval(intervalRef.current);
+        }
+      }, 16);
     }
   };
 
   const onTouchEnd = () => {
-    console.log('End >>>');
-    if (touchedRef.current) {
-      // e.preventDefault();
-      touchedRef.current = false;
+    touchedRef.current = false;
+
+    cleanUpEvents();
+  };
+
+  const onTouchStart = (e: TouchEvent) => {
+    cleanUpEvents();
+
+    if (e.touches.length === 1 && !touchedRef.current) {
+      touchedRef.current = true;
+      touchYRef.current = Math.ceil(e.touches[0].screenY);
+      e.preventDefault();
+
+      elementRef.current = e.target as HTMLElement;
+      elementRef.current.addEventListener('touchmove', onTouchMove);
+      elementRef.current.addEventListener('touchend', onTouchEnd);
+    }
+  };
+
+  cleanUpEvents = () => {
+    if (elementRef.current) {
+      elementRef.current.removeEventListener('touchmove', onTouchMove);
+      elementRef.current.removeEventListener('touchend', onTouchEnd);
     }
   };
 
   React.useEffect(() => {
     listRef.current.addEventListener('touchstart', onTouchStart);
-    window.addEventListener('touchmove', onTouchMove);
-    window.addEventListener('touchend', onTouchEnd);
 
     return () => {
       listRef.current.removeEventListener('touchstart', onTouchStart);
-      window.removeEventListener('touchmove', onTouchMove);
-      window.removeEventListener('touchend', onTouchEnd);
+      cleanUpEvents();
       clearInterval(intervalRef.current);
     };
   }, []);
