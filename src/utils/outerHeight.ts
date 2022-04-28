c089fc5ebcdccdc66992e3e41bc63e4b9bf7f9a5
Flcwl <flcwl@vip.qq.com>
2022-04-28 11:15:27 +0800

    chore: only calc height with marign

--- src/utils/outerHeight.ts
+++ src/utils/outerHeight.ts
@@ -1,5 +1,3 @@
-const isFF = typeof navigator === 'object' && /Firefox/i.test(navigator.userAgent);
-
 /**
  * To get exact height to avoid scrolling deviation
  */
@@ -8,7 +6,5 @@ export const getOuterHeight = (el: HTMLElement) => {
   const computedStyle = window.getComputedStyle(el); 
   height += parseInt(computedStyle.marginTop, 10);
   height += parseInt(computedStyle.marginBottom, 10);
-  height += parseInt(computedStyle.borderTopWidth, 10);
-  height += parseInt(computedStyle.borderBottomWidth, 10);
   return height;
 }
