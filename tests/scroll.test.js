c098457913c383cb831a45dc543c9e7f26121950
linxianxi <904492381@qq.com>
2023-07-13 17:21:19 +0800

    test: revert

--- tests/scroll.test.js
+++ tests/scroll.test.js
@@ -19,9 +19,6 @@ describe('List.Scroll', () => {
       clientHeight: {
         get: () => 100,
       },
-      clientWidth: {
-        get: () => 100,
-      },
     });
   });
 
@@ -40,7 +37,7 @@ describe('List.Scroll', () => {
   function genList(props) {
     let node = (
       <List component="ul" itemKey="id" {...props}>
-        {({ id }) => <li style={{ width: 2000 }}>{id}</li>}
+        {({ id }) => <li>{id}</li>}
       </List>
     );
 
@@ -51,308 +48,264 @@ describe('List.Scroll', () => {
     return mount(node);
   }
 
-  // it('scrollTo null will show the scrollbar', () => {
-  //   jest.useFakeTimers();
-  //   const listRef = React.createRef();
-  //   const wrapper = genList({ itemHeight: 20, height: 100, data: genData(100), ref: listRef });
-  //   jest.runAllTimers();
+  it('scrollTo null will show the scrollbar', () => {
+    jest.useFakeTimers();
+    const listRef = React.createRef();
+    const wrapper = genList({ itemHeight: 20, height: 100, data: genData(100), ref: listRef });
+    jest.runAllTimers();
 
-  //   listRef.current.scrollTo(null);
-  //   expect(wrapper.find('.rc-virtual-list-scrollbar-y-thumb').props().style.display).not.toEqual(
-  //     'none',
-  //   );
-  //   jest.useRealTimers();
-  // });
+    listRef.current.scrollTo(null);
+    expect(wrapper.find('.rc-virtual-list-scrollbar-y-thumb').props().style.display).not.toEqual(
+      'none',
+    );
+    jest.useRealTimers();
+  });
 
-  // describe('scrollTo number', () => {
-  //   it('value scroll', () => {
-  //     const listRef = React.createRef();
-  //     const wrapper = genList({ itemHeight: 20, height: 100, data: genData(100), ref: listRef });
-  //     listRef.current.scrollTo(903);
-  //     jest.runAllTimers();
-  //     expect(wrapper.find('ul').instance().scrollTop).toEqual(903);
+  describe('scrollTo number', () => {
+    it('value scroll', () => {
+      const listRef = React.createRef();
+      const wrapper = genList({ itemHeight: 20, height: 100, data: genData(100), ref: listRef });
+      listRef.current.scrollTo(903);
+      jest.runAllTimers();
+      expect(wrapper.find('ul').instance().scrollTop).toEqual(903);
 
-  //     wrapper.unmount();
-  //   });
-  // });
+      wrapper.unmount();
+    });
+  });
 
-  // describe('scroll to object', () => {
-  //   const listRef = React.createRef();
-  //   const wrapper = genList({ itemHeight: 20, height: 100, data: genData(100), ref: listRef });
+  describe('scroll to object', () => {
+    const listRef = React.createRef();
+    const wrapper = genList({ itemHeight: 20, height: 100, data: genData(100), ref: listRef });
 
-  //   describe('index scroll', () => {
-  //     it('work', () => {
-  //       listRef.current.scrollTo({ index: 30, align: 'top' });
-  //       jest.runAllTimers();
-  //       expect(wrapper.find('ul').instance().scrollTop).toEqual(600);
-  //     });
-
-  //     it('out of range should not crash', () => {
-  //       expect(() => {
-  //         listRef.current.scrollTo({ index: 99999999999, align: 'top' });
-  //         jest.runAllTimers();
-  //       }).not.toThrow();
-  //     });
-  //   });
-
-  //   it('scroll top should not out of range', () => {
-  //     listRef.current.scrollTo({ index: 0, align: 'bottom' });
-  //     jest.runAllTimers();
-  //     expect(wrapper.find('ul').instance().scrollTop).toEqual(0);
-  //   });
-
-  //   it('key scroll', () => {
-  //     listRef.current.scrollTo({ key: '30', align: 'bottom' });
-  //     jest.runAllTimers();
-  //     expect(wrapper.find('ul').instance().scrollTop).toEqual(520);
-  //   });
-
-  //   it('smart', () => {
-  //     listRef.current.scrollTo(0);
-  //     listRef.current.scrollTo({ index: 30 });
-  //     jest.runAllTimers();
-  //     expect(wrapper.find('ul').instance().scrollTop).toEqual(520);
-
-  //     listRef.current.scrollTo(800);
-  //     listRef.current.scrollTo({ index: 30 });
-  //     jest.runAllTimers();
-  //     expect(wrapper.find('ul').instance().scrollTop).toEqual(600);
-  //   });
-  // });
-
-  // it('inject wheel', () => {
-  //   const preventDefault = jest.fn();
-  //   const wrapper = genList({ itemHeight: 20, height: 100, data: genData(100) });
-  //   const ulElement = wrapper.find('ul').instance();
-
-  //   act(() => {
-  //     const wheelEvent = new Event('wheel');
-  //     wheelEvent.deltaY = 3;
-  //     wheelEvent.preventDefault = preventDefault;
-  //     ulElement.dispatchEvent(wheelEvent);
-
-  //     jest.runAllTimers();
-  //   });
-
-  //   expect(preventDefault).toHaveBeenCalled();
-  // });
-
-  // describe('scrollbar', () => {
-  //   it('moving', () => {
-  //     const listRef = React.createRef();
-  //     const wrapper = genList({ itemHeight: 20, height: 100, data: genData(100), ref: listRef });
-
-  //     // Mouse down
-  //     wrapper.find('.rc-virtual-list-scrollbar-y-thumb').simulate('mousedown', {
-  //       pageY: 0,
-  //     });
-
-  //     // Mouse move
-  //     act(() => {
-  //       const mouseMoveEvent = new Event('mousemove');
-  //       mouseMoveEvent.pageY = 10;
-  //       window.dispatchEvent(mouseMoveEvent);
-  //     });
-
-  //     expect(wrapper.find('.rc-virtual-list-holder').props().style.pointerEvents).toEqual('none');
-
-  //     act(() => {
-  //       jest.runAllTimers();
-  //     });
-
-  //     // Mouse up
-  //     act(() => {
-  //       const mouseUpEvent = new Event('mouseup');
-  //       window.dispatchEvent(mouseUpEvent);
-  //     });
-
-  //     expect(wrapper.find('ul').instance().scrollTop > 10).toBeTruthy();
-  //   });
-
-  //   describe('not show scrollbar when disabled virtual', () => {
-  //     [
-  //       { name: '!virtual', props: { virtual: false } },
-  //       {
-  //         name: '!height',
-  //         props: { height: null },
-  //       },
-  //       {
-  //         name: '!itemHeight',
-  //         props: { itemHeight: null },
-  //       },
-  //     ].forEach(({ name, props }) => {
-  //       it(name, () => {
-  //         const wrapper = genList({
-  //           itemHeight: 20,
-  //           height: 100,
-  //           data: genData(5),
-  //           ...props,
-  //         });
-  //         expect(wrapper.find('.rc-virtual-list-scrollbar-y-thumb')).toHaveLength(0);
-  //       });
-  //     });
-  //   });
-  // });
-
-  // it('no bubble', () => {
-  //   const wrapper = genList({ itemHeight: 20, height: 100, data: genData(100) });
-
-  //   // Mouse down
-  //   const preventDefault = jest.fn();
-  //   const stopPropagation = jest.fn();
-  //   wrapper.find('.rc-virtual-list-scrollbar-y').simulate('mousedown', {
-  //     preventDefault,
-  //     stopPropagation,
-  //   });
-
-  //   expect(preventDefault).toHaveBeenCalled();
-  //   expect(stopPropagation).toHaveBeenCalled();
-  // });
-
-  // it('onScroll should trigger on correct target', () => {
-  //   // Save in tmp variable since React will clean up this
-  //   let currentTarget;
-  //   const onScroll = jest.fn(e => {
-  //     ({ currentTarget } = e);
-  //   });
-  //   const wrapper = genList({ itemHeight: 20, height: 100, data: genData(100), onScroll });
-  //   wrapper.find('.rc-virtual-list-holder').simulate('scroll');
-
-  //   expect(currentTarget).toBe(
-  //     wrapper
-  //       .find('.rc-virtual-list-holder')
-  //       .hostNodes()
-  //       .instance(),
-  //   );
-  // });
-
-  // describe('scroll should in range', () => {
-  //   it('less than 0', () => {
-  //     const wrapper = genList({ itemHeight: 20, height: 100, data: genData(100) });
-  //     const ulElement = wrapper.find('ul').instance();
-
-  //     act(() => {
-  //       const wheelEvent = new Event('wheel');
-  //       wheelEvent.deltaY = 9999999;
-  //       ulElement.dispatchEvent(wheelEvent);
-
-  //       jest.runAllTimers();
-  //     });
-
-  //     wrapper.setProps({ data: genData(1) });
-  //     act(() => {
-  //       wrapper
-  //         .find('.rc-virtual-list-holder')
-  //         .props()
-  //         .onScroll({
-  //           currentTarget: {
-  //             scrollTop: 0,
-  //           },
-  //         });
-  //     });
-
-  //     wrapper.setProps({ data: genData(100) });
-
-  //     expect(wrapper.find('ScrollBarY').props().scrollTop).toEqual(0);
-  //   });
-
-  //   it('over max height', () => {
-  //     const wrapper = genList({ itemHeight: 20, height: 100, data: genData(100) });
-  //     const ulElement = wrapper.find('ul').instance();
-
-  //     act(() => {
-  //       const wheelEvent = new Event('wheel');
-  //       wheelEvent.deltaY = 9999999;
-  //       ulElement.dispatchEvent(wheelEvent);
-
-  //       jest.runAllTimers();
-  //     });
-
-  //     wrapper.update();
-
-  //     expect(wrapper.find('ScrollBarY').props().scrollTop).toEqual(1900);
-  //   });
-
-  //   it('dynamic large to small', () => {
-  //     const wrapper = genList({ itemHeight: 20, height: 100, data: genData(1000) });
-  //     const ulElement = wrapper.find('ul').instance();
-
-  //     // To bottom
-  //     act(() => {
-  //       const wheelEvent = new Event('wheel');
-  //       wheelEvent.deltaY = 9999999;
-  //       ulElement.dispatchEvent(wheelEvent);
-
-  //       jest.runAllTimers();
-  //     });
-
-  //     // Cut data len
-  //     wrapper.setProps({
-  //       data: genData(20),
-  //     });
-
-  //     expect(wrapper.find('li').length).toBeLessThan(10);
-  //   });
-  // });
-
-  // it('scrollbar should be left position with rtl', () => {
-  //   jest.useFakeTimers();
-  //   const listRef = React.createRef();
-  //   const wrapper = genList({
-  //     itemHeight: 20,
-  //     height: 100,
-  //     data: genData(100),
-  //     ref: listRef,
-  //     direction: 'rtl',
-  //   });
-  //   jest.runAllTimers();
-
-  //   listRef.current.scrollTo(null);
-  //   expect(wrapper.find('.rc-virtual-list-scrollbar-y-thumb').props().style.display).not.toEqual(
-  //     'none',
-  //   );
-  //   expect(wrapper.find('.rc-virtual-list-scrollbar-y').props().style.left).toEqual(0);
-  //   jest.useRealTimers();
-
-  //   expect(wrapper.exists('.rc-virtual-list-rtl')).toBeTruthy();
-  // });
-
-  describe('horizontal scroll', () => {
-    it('wheel should worked', () => {
-      const wrapper = genList({
-        itemHeight: 20,
-        height: 100,
-        data: genData(100),
-        horizontalScroll: true,
-        style: {
-          width: 50,
-        },
+    describe('index scroll', () => {
+      it('work', () => {
+        listRef.current.scrollTo({ index: 30, align: 'top' });
+        jest.runAllTimers();
+        expect(wrapper.find('ul').instance().scrollTop).toEqual(600);
       });
+
+      it('out of range should not crash', () => {
+        expect(() => {
+          listRef.current.scrollTo({ index: 99999999999, align: 'top' });
+          jest.runAllTimers();
+        }).not.toThrow();
+      });
+    });
+
+    it('scroll top should not out of range', () => {
+      listRef.current.scrollTo({ index: 0, align: 'bottom' });
+      jest.runAllTimers();
+      expect(wrapper.find('ul').instance().scrollTop).toEqual(0);
+    });
+
+    it('key scroll', () => {
+      listRef.current.scrollTo({ key: '30', align: 'bottom' });
+      jest.runAllTimers();
+      expect(wrapper.find('ul').instance().scrollTop).toEqual(520);
+    });
+
+    it('smart', () => {
+      listRef.current.scrollTo(0);
+      listRef.current.scrollTo({ index: 30 });
+      jest.runAllTimers();
+      expect(wrapper.find('ul').instance().scrollTop).toEqual(520);
+
+      listRef.current.scrollTo(800);
+      listRef.current.scrollTo({ index: 30 });
+      jest.runAllTimers();
+      expect(wrapper.find('ul').instance().scrollTop).toEqual(600);
+    });
+  });
+
+  it('inject wheel', () => {
+    const preventDefault = jest.fn();
+    const wrapper = genList({ itemHeight: 20, height: 100, data: genData(100) });
+    const ulElement = wrapper.find('ul').instance();
+
+    act(() => {
+      const wheelEvent = new Event('wheel');
+      wheelEvent.deltaY = 3;
+      wheelEvent.preventDefault = preventDefault;
+      ulElement.dispatchEvent(wheelEvent);
+
+      jest.runAllTimers();
+    });
+
+    expect(preventDefault).toHaveBeenCalled();
+  });
+
+  describe('scrollbar', () => {
+    it('moving', () => {
+      const listRef = React.createRef();
+      const wrapper = genList({ itemHeight: 20, height: 100, data: genData(100), ref: listRef });
+
+      // Mouse down
+      wrapper.find('.rc-virtual-list-scrollbar-y-thumb').simulate('mousedown', {
+        pageY: 0,
+      });
+
+      // Mouse move
+      act(() => {
+        const mouseMoveEvent = new Event('mousemove');
+        mouseMoveEvent.pageY = 10;
+        window.dispatchEvent(mouseMoveEvent);
+      });
+
+      expect(wrapper.find('.rc-virtual-list-holder').props().style.pointerEvents).toEqual('none');
+
+      act(() => {
+        jest.runAllTimers();
+      });
+
+      // Mouse up
+      act(() => {
+        const mouseUpEvent = new Event('mouseup');
+        window.dispatchEvent(mouseUpEvent);
+      });
+
+      expect(wrapper.find('ul').instance().scrollTop > 10).toBeTruthy();
+    });
+
+    describe('not show scrollbar when disabled virtual', () => {
+      [
+        { name: '!virtual', props: { virtual: false } },
+        {
+          name: '!height',
+          props: { height: null },
+        },
+        {
+          name: '!itemHeight',
+          props: { itemHeight: null },
+        },
+      ].forEach(({ name, props }) => {
+        it(name, () => {
+          const wrapper = genList({
+            itemHeight: 20,
+            height: 100,
+            data: genData(5),
+            ...props,
+          });
+          expect(wrapper.find('.rc-virtual-list-scrollbar-y-thumb')).toHaveLength(0);
+        });
+      });
+    });
+  });
+
+  it('no bubble', () => {
+    const wrapper = genList({ itemHeight: 20, height: 100, data: genData(100) });
+
+    // Mouse down
+    const preventDefault = jest.fn();
+    const stopPropagation = jest.fn();
+    wrapper.find('.rc-virtual-list-scrollbar-y').simulate('mousedown', {
+      preventDefault,
+      stopPropagation,
+    });
+
+    expect(preventDefault).toHaveBeenCalled();
+    expect(stopPropagation).toHaveBeenCalled();
+  });
+
+  it('onScroll should trigger on correct target', () => {
+    // Save in tmp variable since React will clean up this
+    let currentTarget;
+    const onScroll = jest.fn((e) => {
+      ({ currentTarget } = e);
+    });
+    const wrapper = genList({ itemHeight: 20, height: 100, data: genData(100), onScroll });
+    wrapper.find('.rc-virtual-list-holder').simulate('scroll');
+
+    expect(currentTarget).toBe(wrapper.find('.rc-virtual-list-holder').hostNodes().instance());
+  });
+
+  describe('scroll should in range', () => {
+    it('less than 0', () => {
+      const wrapper = genList({ itemHeight: 20, height: 100, data: genData(100) });
       const ulElement = wrapper.find('ul').instance();
 
       act(() => {
         const wheelEvent = new Event('wheel');
-        wheelEvent.deltaX = 50;
-        wheelEvent.deltaY = 0;
+        wheelEvent.deltaY = 9999999;
         ulElement.dispatchEvent(wheelEvent);
 
         jest.runAllTimers();
       });
 
-      // expect(
-      //   wrapper
-      //     .find('.rc-virtual-list-holder')
-      //     .find('div')
-      //     .first()
-      //     .instance().scrollLeft,
-      // ).toEqual(0);
-      expect(wrapper.find('List').props()).toEqual({});
-      // expect(
-      //   wrapper
-      //     .find('ScrollBarX')
-      //     .instance()
-      //     .props().scrollLeft,
-      // ).toEqual(10);
+      wrapper.setProps({ data: genData(1) });
+      act(() => {
+        wrapper
+          .find('.rc-virtual-list-holder')
+          .props()
+          .onScroll({
+            currentTarget: {
+              scrollTop: 0,
+            },
+          });
+      });
+
+      wrapper.setProps({ data: genData(100) });
+
+      expect(wrapper.find('ScrollBarY').props().scrollTop).toEqual(0);
     });
+
+    it('over max height', () => {
+      const wrapper = genList({ itemHeight: 20, height: 100, data: genData(100) });
+      const ulElement = wrapper.find('ul').instance();
+
+      act(() => {
+        const wheelEvent = new Event('wheel');
+        wheelEvent.deltaY = 9999999;
+        ulElement.dispatchEvent(wheelEvent);
+
+        jest.runAllTimers();
+      });
+
+      wrapper.update();
+
+      expect(wrapper.find('ScrollBarY').props().scrollTop).toEqual(1900);
+    });
+
+    it('dynamic large to small', () => {
+      const wrapper = genList({ itemHeight: 20, height: 100, data: genData(1000) });
+      const ulElement = wrapper.find('ul').instance();
+
+      // To bottom
+      act(() => {
+        const wheelEvent = new Event('wheel');
+        wheelEvent.deltaY = 9999999;
+        ulElement.dispatchEvent(wheelEvent);
+
+        jest.runAllTimers();
+      });
+
+      // Cut data len
+      wrapper.setProps({
+        data: genData(20),
+      });
+
+      expect(wrapper.find('li').length).toBeLessThan(10);
+    });
+  });
+
+  it('scrollbar should be left position with rtl', () => {
+    jest.useFakeTimers();
+    const listRef = React.createRef();
+    const wrapper = genList({
+      itemHeight: 20,
+      height: 100,
+      data: genData(100),
+      ref: listRef,
+      direction: 'rtl',
+    });
+    jest.runAllTimers();
+
+    listRef.current.scrollTo(null);
+    expect(wrapper.find('.rc-virtual-list-scrollbar-y-thumb').props().style.display).not.toEqual(
+      'none',
+    );
+    expect(wrapper.find('.rc-virtual-list-scrollbar-y').props().style.left).toEqual(0);
+    jest.useRealTimers();
+
+    expect(wrapper.exists('.rc-virtual-list-rtl')).toBeTruthy();
   });
 });
