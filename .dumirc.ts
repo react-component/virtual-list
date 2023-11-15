91124d3ff9a1a9376fd9ff4255c60f2dd7a07beb
DBSDs <379712747@qq.com>
2023-11-16 00:29:42 +0800

    chore: bump dumi@2.x.x

---
+++ .dumirc.ts
@@ -0,0 +1,21 @@
+// more config: https://d.umijs.org/config
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  favicons: ['https://avatars0.githubusercontent.com/u/9441414?s=200&v=4'],
+  themeConfig: {
+    name: 'virtual-list',
+    logo: 'https://avatars0.githubusercontent.com/u/9441414?s=200&v=4',
+  },
+  outputPath: '.doc',
+  base: '/rc-virtual-list/',
+  publicPath: '/rc-virtual-list/',
+  exportStatic: {},
+  styles: [
+    `
+      .markdown table {
+        width: auto !important;
+      }
+    `,
+  ],
+});
--- docs/demo/basic.md
+++ docs/demo/basic.md
@@ -1,3 +1,8 @@
-## basic
+---
+title: basic
+nav:
+  title: Demo
+  path: /demo
+---
 
-<code src="../../examples/basic.tsx">
+<code src="../../examples/basic.tsx"></code>
