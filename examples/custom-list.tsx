5cd7d944145feb9705230ccc55548eae3514de3b
叶枫 <645381995@qq.com>
2024-01-25 16:05:25 +0800

    feat: demo

--- examples/custom-list.tsx
+++ examples/custom-list.tsx
@@ -1,14 +1,30 @@
+/* eslint-disable @typescript-eslint/no-shadow */
 import * as React from 'react';
 import List from '../src/List';
 
 interface Item {
   id: number;
-  noUnMount: boolean;
+  noUnMount?: boolean;
 }
 
-const MyItem: React.ForwardRefRenderFunction<HTMLElement, Item> = ({ id }, ref) => {
+const MyItem: React.ForwardRefRenderFunction<HTMLElement, { item?: Item; onOpen?: () => any }> = (
+  { item: { id, noUnMount }, onOpen },
+  ref,
+) => {
+  React.useEffect(() => {
+    return () => {
+      if (noUnMount) {
+        console.log('111');
+      }
+    };
+  }, [noUnMount]);
+
   return (
-    <span ref={ref} style={{ border: '1px solid gray', padding: '0 16px' }}>
+    <span
+      ref={ref}
+      style={{ border: '1px solid', background: noUnMount ? 'red' : undefined }}
+      onClick={() => onOpen()}
+    >
       {id}
     </span>
   );
@@ -18,14 +34,16 @@ const ForwardMyItem = React.forwardRef(MyItem);
 
 const data: Item[] = [];
 for (let i = 0; i < 10; i += 1) {
-  data.push({ id: i, noUnMount: i === 9 || i === 0 });
+  // data.push({ id: i, noUnMount: i === 9 || i === 0 });
+  data.push({ id: i });
 }
 
 const Demo = () => {
+  const [list, setList] = React.useState(data);
   return (
     <React.StrictMode>
       <List
-        data={data}
+        data={list}
         height={100}
         itemHeight={30}
         itemKey="id"
@@ -39,7 +57,16 @@ const Demo = () => {
           return [...baseList, ...noMountList];
         }}
       >
-        {(item) => <ForwardMyItem {...item} />}
+        {(item) => (
+          <ForwardMyItem
+            item={item}
+            onOpen={() =>
+              setList((list) =>
+                list.map((item2) => ({ ...item2, noUnMount: item2.id === item.id })),
+              )
+            }
+          />
+        )}
       </List>
     </React.StrictMode>
   );
