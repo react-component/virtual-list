0dee58563839c0e0bc15d9d082b054b70901c648
linxianxi <904492381@qq.com>
2024-05-22 11:09:11 +0800

    feat: support touch horizontal scroll

--- src/hooks/useMobileTouchMove.ts
+++ src/hooks/useMobileTouchMove.ts
@@ -1,15 +1,16 @@
-import * as React from 'react';
-import { useRef } from 'react';
 import useLayoutEffect from 'rc-util/lib/hooks/useLayoutEffect';
+import type * as React from 'react';
+import { useRef } from 'react';
 
 const SMOOTH_PTG = 14 / 15;
 
 export default function useMobileTouchMove(
   inVirtual: boolean,
   listRef: React.RefObject<HTMLDivElement>,
-  callback: (offsetY: number, smoothOffset?: boolean) => boolean,
+  callback: (isHorizontal: boolean, offset: number, smoothOffset?: boolean) => boolean,
 ) {
   const touchedRef = useRef(false);
+  const touchXRef = useRef(0);
   const touchYRef = useRef(0);
 
   const elementRef = useRef<HTMLElement>(null);
@@ -22,20 +23,30 @@ export default function useMobileTouchMove(
 
   const onTouchMove = (e: TouchEvent) => {
     if (touchedRef.current) {
+      const currentX = Math.ceil(e.touches[0].pageX);
       const currentY = Math.ceil(e.touches[0].pageY);
+      let offsetX = touchXRef.current - currentX;
       let offsetY = touchYRef.current - currentY;
-      touchYRef.current = currentY;
-
-      if (callback(offsetY)) {
-        e.preventDefault();
+      const isHorizontal = Math.abs(offsetX) >= Math.abs(offsetY);
+      if (isHorizontal) {
+        touchXRef.current = currentX;
+      } else {
+        touchYRef.current = currentY;
       }
 
+      if (callback(isHorizontal, isHorizontal ? offsetX : offsetY)) {
+        e.preventDefault();
+      }
       // Smooth interval
       clearInterval(intervalRef.current);
       intervalRef.current = setInterval(() => {
-        offsetY *= SMOOTH_PTG;
-
-        if (!callback(offsetY, true) || Math.abs(offsetY) <= 0.1) {
+        if (isHorizontal) {
+          offsetX *= SMOOTH_PTG;
+        } else {
+          offsetY *= SMOOTH_PTG;
+        }
+        const offset = isHorizontal ? offsetX : offsetY;
+        if (!callback(isHorizontal, offset, true) || Math.abs(offset) <= 0.1) {
           clearInterval(intervalRef.current);
         }
       }, 16);
@@ -53,6 +64,7 @@ export default function useMobileTouchMove(
 
     if (e.touches.length === 1 && !touchedRef.current) {
       touchedRef.current = true;
+      touchXRef.current = Math.ceil(e.touches[0].pageX);
       touchYRef.current = Math.ceil(e.touches[0].pageY);
 
       elementRef.current = e.target as HTMLElement;
