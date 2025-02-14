78ddcf3bd67bdacbe433539121c48ddbc6837007
二货机器人 <smith3816@gmail.com>
2025-02-14 16:06:20 +0800

    fix: draggable

--- src/hooks/useScrollDrag.ts
+++ src/hooks/useScrollDrag.ts
@@ -39,6 +39,10 @@ export default function useScrollDrag(
       };
 
       const onMouseDown = (e: MouseEvent) => {
+        // 如果目标元素设置了 draggable，则跳过滚动逻辑
+        if ((e.target as HTMLElement).draggable) {
+          return;
+        }
         // Skip if nest List has handled this event
         const event = e as MouseEvent & {
           _virtualHandled?: boolean;
